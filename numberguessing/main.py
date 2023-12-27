a4ae01c3388591acfd025236ad6b0e17bd91d152
Amina H <amina01hu@gmail.com>
2023-12-27 17:17:49 -0500

    add number guessing project

---
+++ numberguessing/main.py
@@ -0,0 +1,62 @@
+import random
+from pyfiglet import figlet_format
+from time import sleep
+from termcolor import colored, cprint
+import os
+import sys
+import time
+
+def loading_animation():
+    for _ in range(3):
+        for _ in range(3):  # Adjust the range based on the number of dots you want
+            sys.stdout.write("Number Loading" + "." * (_ % 3 + 1) + "\r")
+            sys.stdout.flush()
+            time.sleep(0.5)  # Adjust the sleep duration as needed
+        sys.stdout.write(" " * 20 + "\r")
+        sys.stdout.flush()
+    cprint("\nThank you d(-_☆)\n", 'blue')
+
+
+def titleSequence():
+    cprint("*****************************************************************", 'blue')
+    title = figlet_format("Number Guessing Game", font="speed", justify="center", width=70)
+    cprint(title, 'blue')
+    cprint("*****************************************************************", 'blue')
+    cprint("\n------- This is a Number Guessing Game that uses Python  -------", 'blue')
+    cprint("\nThe rules are simple, you will pick a range for the number and \nI will tell you based on your input if you are correct or not!", 'yellow')
+    cprint("\n*** LETS BEGIN (⌐▨_▨) \n", 'blue')
+
+def defineNumber():
+    while True:
+        try:
+            guessRange = int(input("Please pick the range of the number: "))
+            number = random.randrange(1, guessRange)
+            break
+        except ValueError:
+            print("Sorry, that is not a valid number!\n")
+    return number
+
+def guessTheNumber(number):
+    selection = 0
+    while selection != number:
+        selection = int(input("What number am I thinking of: "))
+        if selection < number:
+            print("too low")
+
+
+titleSequence()
+guessTheNumber(defineNumber())
+
+
+
+"""loading_animation()"""
+
+
+
+
+
+
+
+
+
+
